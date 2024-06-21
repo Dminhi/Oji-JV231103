e018dfcbd78f31af0b281f75cb7bd9534875c034
tanquynh <tanquynh1605@gmail.com>
2024-06-21 16:24:00 +0700

    Experience

--- src/main/java/com/example/ojt/controller/candidate/ExperienceCandidateController.java
+++ src/main/java/com/example/ojt/controller/candidate/ExperienceCandidateController.java
@@ -3,6 +3,7 @@ package com.example.ojt.controller.candidate;
 import com.example.ojt.exception.CustomException;
 import com.example.ojt.model.dto.request.EduCandidateRequestDTO;
 import com.example.ojt.model.dto.request.ExperienceCandidateRequestDTO;
+import com.example.ojt.model.dto.request.ProjectCandidateRequestDTO;
 import com.example.ojt.model.dto.response.APIResponse;
 import com.example.ojt.service.eduCandidate.IEduCandidateService;
 import com.example.ojt.service.experienceCandidate.IExperienceCandidateService;
@@ -10,10 +11,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api.myservice.com/v1/account/ex-candidate")
@@ -25,6 +23,16 @@ public class ExperienceCandidateController {
         boolean check = experienceCandidateService.saveOrUpdate(experienceCandidateRequestDTO);
         if(check) {
             APIResponse apiResponse = new APIResponse(200, "Create Experience candidate success");
+            return new ResponseEntity<>(apiResponse, HttpStatus.CREATED);
+        }else {
+            throw new CustomException("Lack of compulsory registration information or invalid information.", HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+    }
+    @PutMapping("")
+    ResponseEntity<?> EditEduCandidate(@Valid @RequestBody ExperienceCandidateRequestDTO experienceCandidateRequestDTO) throws CustomException {
+        boolean check = experienceCandidateService.saveOrUpdate(experienceCandidateRequestDTO);
+        if(check) {
+            APIResponse apiResponse = new APIResponse(200, "Update Experience candidate success");
             return new ResponseEntity<>(apiResponse, HttpStatus.CREATED);
         }else {
             throw new CustomException("Lack of compulsory registration information or invalid information.", HttpStatus.UNPROCESSABLE_ENTITY);
