22708f511eba7ce1b239f25d9823ad84505436c2
tanquynh <tanquynh1605@gmail.com>
2024-06-21 16:05:01 +0700

    Task-4953-update-projectCandidate

--- src/main/java/com/example/ojt/controller/candidate/ProjectCandidateController.java
+++ src/main/java/com/example/ojt/controller/candidate/ProjectCandidateController.java
@@ -10,10 +10,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api.myservice.com/v1/account/pro-candidate")
@@ -25,6 +22,16 @@ public class ProjectCandidateController {
         boolean check = productCandidateService.saveOrUpdate(projectCandidateRequestDTO);
         if(check) {
             APIResponse apiResponse = new APIResponse(200, "Create Education candidate success");
+            return new ResponseEntity<>(apiResponse, HttpStatus.CREATED);
+        }else {
+            throw new CustomException("Lack of compulsory registration information or invalid information.", HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+    }
+    @PutMapping("")
+    ResponseEntity<?> EditEduCandidate(@Valid @RequestBody ProjectCandidateRequestDTO projectCandidateRequestDTO) throws CustomException {
+        boolean check = productCandidateService.saveOrUpdate(projectCandidateRequestDTO);
+        if(check) {
+            APIResponse apiResponse = new APIResponse(200, "Update Project candidate success");
             return new ResponseEntity<>(apiResponse, HttpStatus.CREATED);
         }else {
             throw new CustomException("Lack of compulsory registration information or invalid information.", HttpStatus.UNPROCESSABLE_ENTITY);
